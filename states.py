0f3be85fa122ca02aad167bf485c58daf6ddb273
Henri <henri-leon.kordt@studium.uni-hamburg.de>
2023-12-15 11:29:45 +0100

    pseudo code and some structure

--- states.py
+++ states.py
@@ -2,6 +2,7 @@ from FeatureCloud.app.engine.app import AppState, app_state, Role
 import time
 import pandas as pd
 import os
+from scipy.spatial import distance
 
 # FeatureCloud requires that apps define the at least the 'initial' state.
 # This state is executed after the app instance is started.
@@ -17,6 +18,9 @@ class InitialState(AppState):
         # https://featurecloud.ai/assets/developer_documentation/getting_started.html
         dataFile = os.path.join(os.getcwd(), "mnt", "input", "data.csv")
         data = pd.read_csv(dataFile)
+        #replace data loading here
+        
+
         self.store(key="data", value=data)
         
         return 'calculation'  
@@ -29,44 +33,86 @@ class CalculationState(AppState):
 
     def register(self):
         self.register_transition('terminal')
-        self.register_transition('aggregate', role=Role.COORDINATOR)  
+        self.register_transition('AggregateSpikePoints', role=Role.COORDINATOR)  
+        self.register_transition('recieveSpikePoints')
         # We declare that 'terminal' state is accessible from the 'initial' state.
 
     def run(self):
         # Checkout our documentation for help on how to implement an app
         # https://featurecloud.ai/assets/developer_documentation/getting_started.html
         data = self.load("data")
-        mean = data["salary"].sum()
-        n = len(data["salary"])
-        self.send_data_to_coordinator((mean, n), 
+        #run kmeans on data
+        #import sklearn
+        #from sklearn.cluster import KMeans
+        kmeans = KMeans(n_clusters=2, random_state=0).fit(data)
+        self.send_data_to_coordinator(kmeans.cluster_centers_, 
                                       send_to_self=True, 
                                       use_smpc=False, 
                                       use_dp=False)
         if self.is_coordinator:
-            return "aggregate"
+            return "AggregateSpikePoints"
         else:
             return "terminal"
         
+@app_state("recieveSpikePoints")
+class recieveSpikePointsState(AppState):
+    def register(self):
+        self.register_transition('terminal')
+    def run(self):
+        data = await_data(n: int = 1, unwrap=True, is_json=False)
+        #calulate LSDM (S x N)
+        # for each spike point calculate distance to all data points
+        own_data = load("data")
+        #either implement wiht loop
+        #or better with one function
+        for spikePoint in data:
+            for dataPoint in own_data:
 
-@app_state('aggregate')
-class AggregateState(AppState):
+                #calculate distance
+                #store distance
+                pass
+
+        #construct PEDM
+        # something with regression !
+        #share results:
+        self.send_data_to_coordinator(kmeans.cluster_centers_, 
+                                      send_to_self=True, 
+                                      use_smpc=False, 
+                                      use_dp=False)
+
+        return "terminal"
+
+@app_state('AggregateSpikePoints')
+class AggregateSpikePointsState(AppState):
 
     def register(self):
         self.register_transition('terminal')
+        self.register_transition('recieveSpikePoints')
         # We declare that 'terminal' state is accessible from the 'initial' state.
 
     def run(self):
         # Checkout our documentation for help on how to implement an app
         # https://featurecloud.ai/assets/developer_documentation/getting_started.html
         aggData = self.gather_data(use_smpc=False, use_dp=False)
-        total_agg = 0
-        total_n = 0
-        for client_data in aggData:
-            agg = client_data[0]
-            n = client_data[1]
-            total_agg += agg
-            total_n += n
-        resFile = os.path.join(os.getcwd(), "mnt", "output", "result.txt")
-        with open(resFile, "w") as f:
-            f.write(str(total_agg/total_n) + "\n")
-        return "terminal"
+
+        #figure out shape of aggData
+        #redistribute to participants
+        broadcast_data(data, send_to_self=True    , use_dp=False )
+
+
+        return "recieveSpikePoints"
+
+
+
+        #calulate LSDM
+
+        #construct PEDM
+
+
+@app_state('wrapup')
+class wrapupState(AppState)
+    def register(self):
+        self.register_transition('terminal')
+    def run(self):
+        aggData = self.gather_data(use_smpc=False, use_dp=False)
+        #perform warp up
